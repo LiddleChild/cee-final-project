6ea97e41e62c3f91877c151e4956c5635dd203c7
oreo10baht <papigonwin001@gmail.com>
2023-04-26 00:09:38 +0700

    จัดหน้า

--- frontend/script_cv.js
+++ frontend/script_cv.js
@@ -1,170 +1,201 @@
+/** @format */
+
 const backendIPAddress = "localhost:3000";
 //----------------------Buttons------------------------
-let logoutButton = document.getElementById("logout"); 
+let logoutButton = document.getElementById("logout");
 let loginButton = document.getElementById("login");
 //-----------------------------------------------------
 let monthHeader = document.getElementById("monthyear");
-let monthLi = {"Jan": ["January",31], "Feb": ["February",28], "Mar": ["March",31], "Apr": ["April",30],  "May": ["May",31],  "Jun": ["June",30], "Jul": ["July", 31],  "Aug": ["August",31], "Sep": ["September",30],"Oct": ["October",31], "Nov": ["November",30], "Dec": ["December",31]}
-let dayUl = document.getElementById("dayUl")
+let monthLi = {
+  Jan: ["January", 31],
+  Feb: ["February", 28],
+  Mar: ["March", 31],
+  Apr: ["April", 30],
+  May: ["May", 31],
+  Jun: ["June", 30],
+  Jul: ["July", 31],
+  Aug: ["August", 31],
+  Sep: ["September", 30],
+  Oct: ["October", 31],
+  Nov: ["November", 30],
+  Dec: ["December", 31],
+};
+let dayUl = document.getElementById("dayUl");
 var userInfo = undefined;
 //----------------------Date---------------------------
 let d = new Date();
-let chosenDate = d.toString().split(" ")
-var firstDayOfMonth = new Date(monthLi[chosenDate[1]][0] + " 1, " + chosenDate[3] + " 00:00:00")
-let currentDate = firstDayOfMonth.toString().split(" ")
-var firstWeekday = firstDayOfMonth.getDay()
+let chosenDate = d.toString().split(" ");
+var firstDayOfMonth = new Date(
+  monthLi[chosenDate[1]][0] + " 1, " + chosenDate[3] + " 00:00:00"
+);
+let currentDate = firstDayOfMonth.toString().split(" ");
+var firstWeekday = firstDayOfMonth.getDay();
 //-----------------------------------------------------
 var calendar;
 
-getCalendar()
-console.log(firstWeekday)
+getCalendar();
+console.log(firstWeekday);
 
-function getCalendar(){
+function getCalendar() {
   fetch(
-    `http://localhost:3000/api/get_calendar?month=${firstDayOfMonth.getMonth() + 1}&year=${firstDayOfMonth.getFullYear()}`, {
+    `http://localhost:3000/api/get_calendar?month=${
+      firstDayOfMonth.getMonth() + 1
+    }&year=${firstDayOfMonth.getFullYear()}`,
+    {
       method: "GET",
       credentials: "include",
     }
   )
-    .then(res => res.json())
-    .then(data => {
-      calendar = data
-      
+    .then((res) => res.json())
+    .then((data) => {
+      calendar = data;
     })
-    .then(() =>{
-      console.log(calendar)
-    })
-     
+    .then(() => {
+      console.log(calendar);
+    });
 }
 
-function updateSidebar(){
-  
-  console.log(chosenDate[2])
-  document.getElementsByClassName("date")[0].innerHTML = chosenDate[2] + " " + chosenDate[1] + " " + chosenDate[3]
-  var itemList = document.getElementsByClassName("items")[0]
-      while(itemList.firstChild) itemList.removeChild(itemList.firstChild)
-      var chosenAssignmentList = []
+function updateSidebar() {
+  console.log(chosenDate[2]);
+  document.getElementsByClassName("date")[0].innerHTML =
+    chosenDate[2] + " " + chosenDate[1] + " " + chosenDate[3];
+  var itemList = document.getElementsByClassName("items")[0];
+  while (itemList.firstChild) itemList.removeChild(itemList.firstChild);
+  var chosenAssignmentList = [];
 
-      if(chosenDate[2] in calendar) {
-        chosenAssignmentList = calendar[chosenDate[2]]
-        for(let i = 0; i < chosenAssignmentList.length; i++){
-          var item = document.createElement("li")
-          if((i > 0 && chosenAssignmentList[i]["course_no"] != chosenAssignmentList[i - 1]["course_no"]) || i == 0){
-            var subject = document.createElement("h2")
-            subject.innerHTML = chosenAssignmentList[i]["course_title"]
-            console.log(subject)
-            itemList.appendChild(subject)
-          }
-          var dueDate = new Date(chosenAssignmentList[i]["assignment_duetime"]*1000);
-          item.innerHTML = dueDate.toTimeString().split(" ")[0] + ": " + chosenAssignmentList[i]["assignment_title"] 
-          itemList.appendChild(item)
+  if (chosenDate[2] in calendar) {
+    chosenAssignmentList = calendar[chosenDate[2]];
+    for (let i = 0; i < chosenAssignmentList.length; i++) {
+      var item = document.createElement("li");
+      if (
+        (i > 0 &&
+          chosenAssignmentList[i]["course_no"] !=
+            chosenAssignmentList[i - 1]["course_no"]) ||
+        i == 0
+      ) {
+        var subject = document.createElement("h2");
+        subject.innerHTML = chosenAssignmentList[i]["course_title"];
+        console.log(subject);
+        itemList.appendChild(subject);
       }
-
-      }
-      else{
-        var none = document.createElement("li")
-        none.innerHTML = "Nothing to do today!"
-        itemList.appendChild(none)
-      }
-  
-
-}
-async function updateCal() { 
-  getCalendar()
-  const res = await fetch(`http://localhost:3000/api/get_calendar?month=${firstDayOfMonth.getMonth() + 1}&year=${firstDayOfMonth.getFullYear()}`)
-  while(dayUl.firstChild){
-    dayUl.removeChild(dayUl.firstChild)
-  }
-  monthHeader.innerHTML = monthLi[currentDate[1]][0] + " " + currentDate[3]
-  
-  addEmptyDays()
-  
-  for(let i = 1; i <= monthLi[currentDate[1]][1]; i++){
-    var li = document.createElement("li")
-    li.appendChild(document.createTextNode(i))
-    if(calendar[i] != undefined) li.style.color = "red"
-    li.onclick = function() {chosenDate[2] = i; updateSidebar()}
-    dayUl.appendChild(li)
-    
-  }
-  updateSidebar()
-}
-  
-  
-function addEmptyDays(){
-  if(firstWeekday == 0) firstWeekday = 7;
-  for(let i = 1; i < firstWeekday; i++ ){
-    var empty = document.createElement("li")
-    empty.style.borderStyle = "hidden"
-    empty.style.paddingRight = "1.6px"
-    empty.style.borderWidth = "0.5px"
-    dayUl.appendChild(empty)
+      var dueDate = new Date(
+        chosenAssignmentList[i]["assignment_duetime"] * 1000
+      );
+      item.innerHTML =
+        dueDate.toTimeString().split(" ")[0] +
+        ": " +
+        chosenAssignmentList[i]["assignment_title"];
+      itemList.appendChild(item);
+    }
+  } else {
+    var none = document.createElement("li");
+    none.innerHTML = "Nothing to do today!";
+    itemList.appendChild(none);
   }
 }
+async function updateCal() {
+  getCalendar();
+  const res = await fetch(
+    `http://localhost:3000/api/get_calendar?month=${
+      firstDayOfMonth.getMonth() + 1
+    }&year=${firstDayOfMonth.getFullYear()}`
+  );
+  while (dayUl.firstChild) {
+    dayUl.removeChild(dayUl.firstChild);
+  }
+  monthHeader.innerHTML = monthLi[currentDate[1]][0] + " " + currentDate[3];
 
-function nextMonth(){
+  addEmptyDays();
+
+  for (let i = 1; i <= monthLi[currentDate[1]][1]; i++) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(i));
+    if (calendar[i] != undefined) li.style.color = "red";
+    li.onclick = function () {
+      chosenDate[2] = i;
+      updateSidebar();
+    };
+    dayUl.appendChild(li);
+  }
+  updateSidebar();
+}
+
+function addEmptyDays() {
+  if (firstWeekday == 0) firstWeekday = 7;
+  for (let i = 1; i < firstWeekday; i++) {
+    var empty = document.createElement("li");
+    empty.style.borderStyle = "hidden";
+    empty.style.paddingRight = "1.6px";
+    empty.style.borderWidth = "0.5px";
+    dayUl.appendChild(empty);
+  }
+}
+
+function nextMonth() {
   if (firstDayOfMonth.getMonth() == 11) {
     firstDayOfMonth = new Date(firstDayOfMonth.getFullYear() + 1, 0, 1);
-} else {
-    firstDayOfMonth = new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth() + 1, 1);            
-}
-currentDate = firstDayOfMonth.toString().split(" ")
-firstWeekday = firstDayOfMonth.getDay()
-chosenDate = currentDate
-updateCal()
+  } else {
+    firstDayOfMonth = new Date(
+      firstDayOfMonth.getFullYear(),
+      firstDayOfMonth.getMonth() + 1,
+      1
+    );
+  }
+  currentDate = firstDayOfMonth.toString().split(" ");
+  firstWeekday = firstDayOfMonth.getDay();
+  chosenDate = currentDate;
+  updateCal();
 }
 
-function prevMonth(){
+function prevMonth() {
   if (firstDayOfMonth.getMonth() == 1) {
     firstDayOfMonth = new Date(firstDayOfMonth.getFullYear() - 1, 12, 1);
-} else {
-    firstDayOfMonth = new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth() - 1, 1);            
+  } else {
+    firstDayOfMonth = new Date(
+      firstDayOfMonth.getFullYear(),
+      firstDayOfMonth.getMonth() - 1,
+      1
+    );
+  }
+  currentDate = firstDayOfMonth.toString().split(" ");
+  firstWeekday = firstDayOfMonth.getDay();
+  chosenDate = currentDate;
+  updateCal();
 }
-currentDate = firstDayOfMonth.toString().split(" ")
-firstWeekday = firstDayOfMonth.getDay()
-chosenDate = currentDate
-updateCal()
-}
-
 
 function toggleButton() {
-    fetch("http://localhost:3000/courseville/get_user_info", {
+  fetch("http://localhost:3000/courseville/get_user_info", {
     method: "GET",
     credentials: "include",
   })
     .then((response) => response.json())
-    .then((json) => { 
+    .then((json) => {
       console.log(json);
-      userInfo = json.data
-      console.log(userInfo)
-      if(userInfo == undefined){
-        loginButton.style.display = "block"
-        logoutButton.style.display = "none"
+      userInfo = json.data;
+      console.log(userInfo);
+      if (userInfo == undefined) {
+        loginButton.style.display = "block";
+        logoutButton.style.display = "none";
+      } else {
+        loginButton.style.display = "none";
+        logoutButton.style.display = "block";
       }
-      else{
-        loginButton.style.display = "none"
-        logoutButton.style.display = "block"
-      }
-
     })
     .catch((err) => {
-      console.error(err);     
+      console.error(err);
     });
-  }
-  
-toggleButton()
+}
+
+toggleButton();
 
 function login() {
   window.location.href = `http://${backendIPAddress}/courseville/login`;
-  toggleButton()
-
-};
-
-function logout() {
-  window.location.href =  `http://${backendIPAddress}/courseville/logout`;
-  toggleButton()
+  toggleButton();
 }
 
-updateCal()
+function logout() {
+  window.location.href = `http://${backendIPAddress}/courseville/logout`;
+  toggleButton();
+}
 
-
+updateCal();
